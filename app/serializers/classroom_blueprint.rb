7843a5f78b86b119cfe113a13068b777aef3706a
Niranjan Patil <niranjan.patil.-nd@disney.com>
2025-04-14 23:58:00 +0530

    Classroom name change

--- app/serializers/classroom_blueprint.rb
+++ app/serializers/classroom_blueprint.rb
@@ -1,9 +1,13 @@
 class ClassroomBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :name, :batch, :year
+  fields :batch, :year
 
   field :teacher_name do |classroom|
     classroom.teacher&.name
+  end
+
+  field :name do |classroom|
+    classroom.name + classroom.batch + classroom.year
   end
 end
